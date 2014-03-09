d602ba00158e87aa8477dc49278aa5347bfd05e9
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-03-10 01:33:03 +0300

    add necessary middlewares and index route

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
-var app, koa;
+var app, json, koa, logger, parse, render, router, serve, session, views;
 
 koa = require('koa');
 
+logger = require('koa-logger');
+
+router = require('koa-router');
+
+serve = require('koa-static');
+
+parse = require('co-body');
+
+views = require('co-views');
+
+json = require('koa-json');
+
+session = require('koa-session');
+
 app = koa();
 
-app.use(function*() {
-  return this.body = 'Hello World';
+app.use(logger());
+
+app.use(json());
+
+app.use(session());
+
+app.use(router(app));
+
+render = views('views/');
+
+app.use(serve('./public'));
+
+require('koa-qs')(app);
+
+app.get('/', function*() {
+  return this.body = yield render('index.jade');
+});
+
+app.get('/api/:id', function*() {
+  return this.body = {
+    id: this.params.id,
+    query: this.query
+  };
 });
 
 app.listen(3000);
--- src/app.coffee
+++ src/app.coffee
@@ -1,7 +1,28 @@
 koa = require 'koa'
+logger = require 'koa-logger'
+router = require 'koa-router'
+serve = require 'koa-static'
+parse = require 'co-body'
+views = require 'co-views'
+json = require 'koa-json'
+session = require 'koa-session'
+
 app = koa()
 
-app.use ->*
-  @body = 'Hello World'
+app.use logger()
+app.use json()
+app.use session()
+app.use router app
+render = views 'views/'
+app.use serve './public'
+require('koa-qs')(app)
+
+app.get '/', ->*
+  @body = yield render 'index.jade'
+
+app.get '/api/:id', ->*
+  @body =
+    id: @params.id
+    query: @query
 
 app.listen 3000
